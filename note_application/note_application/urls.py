bfc705fb08aa64416ebde2307b52ea14f30e7772
Paul Mubanga <159537964+X3KSIS@users.noreply.github.com>
2026-07-16 21:27:11 +0200

    Include notes.urls in the main URL configuration

--- note_application/note_application/urls.py
+++ note_application/note_application/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from notes.views import index
+from django.urls import path, include
 
 urlpatterns = [
-    path('', index, name='name'),
     path('admin/', admin.site.urls),
+    path('', include('notes.urls')), 
 ]
