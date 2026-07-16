cb19832b5a6b37c3e8afd8e43c976f9cef4ca38d
Paul Mubanga <159537964+X3KSIS@users.noreply.github.com>
2026-07-16 21:33:45 +0200

    Implement CRUD views for Notes with authentication

--- note_application/notes/views.py
+++ note_application/notes/views.py
@@ -1,5 +1,53 @@
 from django.shortcuts import render
-from notes.models import Notes
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from .models import Notes
+from .forms import NoteForm
 
-def index(request):
-    return render(request, 'notes/index.html')
+
+class NoteListView(LoginRequiredMixin, ListView):
+    model = Notes
+    template_name = 'notes/index.html'
+    context_object_name = 'notes'
+    ordering = ['-created']
+
+    def get_queryset(self):
+        return Notes.objects.filter(user=self.request.user)
+
+
+class NoteCreateView(LoginRequiredMixin, CreateView):
+    model = Notes
+    form_class = NoteForm
+    template_name = 'notes/note_form.html'
+    success_url = reverse_lazy('notes:list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class NoteDetailView(LoginRequiredMixin, DetailView):
+    model = Notes
+    template_name = 'notes/note_detail.html'
+    context_object_name = 'note'
+
+    def get_queryset(self):
+        return Notes.objects.filter(user=self.request.user)
+
+class NoteUpdateView(LoginRequiredMixin, UpdateView):
+    model = Notes
+    form_class = NoteForm
+    template_name = 'notes/note_form.html'
+    success_url = reverse_lazy('notes:list')
+
+    def get_queryset(self):
+        return Notes.objects.filter(user=self.request.user)
+
+
+class NoteDeleteView(LoginRequiredMixin, DeleteView):
+    model = Notes
+    template_name = 'notes/note_confirm_delete.html'
+    success_url = reverse_lazy('notes:list')
+
+    def get_queryset(self):
+        return Notes.objects.filter(user=self.request.user)
